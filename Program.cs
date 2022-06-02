a1052843153e1bf6f262c5105e3c19279ec09656
shxrkyy <86849180+shxrkyy@users.noreply.github.com>
2022-06-02 09:56:16 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,29 +6,13 @@ namespace Terminator
     {
         static void Main(string[] args)
         {
-            // Appearance
-
-            Console.Title = "Terminator v0.0.1 PRE-ALPHA";
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WindowHeight = 40;
-
-            // Login
-
-            Console.WriteLine("What is your username?");
-
-            string userName = Console.ReadLine();
-
-            Console.WriteLine("What is your password?");
-
-            string password = Console.ReadLine();
-
-            // Welcome
-
-            Console.WriteLine("Welcome, " + userName + " to Terminator!\n Version: 0.0.1 PRE-ALPHA (Up to Date)");
-
-            // Console
-
-            Console.ReadKey();
+   
+            // console stuff
+            Console.Title = "idk";
+            Console.ForegroundColor = Blue;
+            Console.WindowHeight = 13;
+            
+            
         }
     }
 }
